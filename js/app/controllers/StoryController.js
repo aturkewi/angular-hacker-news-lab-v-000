d63662a65f09cd98c813a594b44d6ba447747a71
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-06-26 11:00:49 -0400

    Comment out story controller

--- js/app/controllers/StoryController.js
+++ js/app/controllers/StoryController.js
@@ -1,8 +1,8 @@
-function StoryController($scope, article){
-  // debugger;
-  $scope.storyData = article.data
-}
-
-angular
-  .module('app')
-  .controller('StoryController', StoryController)
+// function StoryController($scope, article){
+//   // debugger;
+//   $scope.storyData = article.data
+// }
+//
+// angular
+//   .module('app')
+//   .controller('StoryController', StoryController)
